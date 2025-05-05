6a8ef1cb1b910a65b6a7f6ac1104ad0be309742b
T8020Token <anthony@t8020consulting.com>
2025-05-05 18:44:16 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Home from '../crossmint-integration/Home';
 export default function Index() {
     return (
         <div style={{ textAlign: 'center', padding: '20px' }}>
-            <h1>T8020TOKEN Minting</h1>
+            <h1>T8020 National Membership Token</h1>
             <Home />
         </div>
     );
